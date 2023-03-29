58dea9c36d6acaad4127ced0ceaebe453d859229
Jorge <jorge.velarde0422@gmail.com>
2023-03-29 17:46:23 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, value) {
+    let messagedNames = []
+    for (let i = 0; i < names.length; i++) {
+        messagedNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);    
+    }
+
+    return messagedNames;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number --
+    }
+}
